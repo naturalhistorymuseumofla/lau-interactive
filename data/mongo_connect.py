be759881a4c7858773e5b85b37c1f409f9fb3ae0
dmarkbreiter <58999191+dmarkbreiter@users.noreply.github.com>
2021-05-17 12:02:41 -0700

    Added print to stdout

--- data/mongo_connect.py
+++ data/mongo_connect.py
@@ -1,6 +1,7 @@
 from mongoengine import connect
 import os
 from dotenv import load_dotenv
+import sys
 
 
 load_dotenv()
@@ -8,5 +9,6 @@ DB_URI = os.getenv('DB_URI')
 
 
 def global_init():
+    print('This is standard output', file=sys.stdout)
     connect(alias='laumap', host=DB_URI)
     #mongoengine.register_connection(alias='lau-prototype', name='lau-prototype')
