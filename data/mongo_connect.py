9e3c3adf13df98c410982629f88afffe0b1a5e0e
dmarkbreiter <58999191+dmarkbreiter@users.noreply.github.com>
2021-05-17 12:51:19 -0700

    Refactor

--- data/mongo_connect.py
+++ data/mongo_connect.py
@@ -4,11 +4,9 @@ from dotenv import load_dotenv
 import sys
 
 
-load_dotenv()
-DB_URI = os.getenv('DB_URI')
-
-
 def global_init():
+    load_dotenv()
+    DB_URI = os.getenv('DB_URI')
     print('This is standard output', file=sys.stdout)
     connect(alias='laumap', host=DB_URI)
     #mongoengine.register_connection(alias='lau-prototype', name='lau-prototype')
