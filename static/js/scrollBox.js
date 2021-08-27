b1f5816458bbe9f1fb321d35209fa05b1f2928d5
dmarkbreiter <58999191+dmarkbreiter@users.noreply.github.com>
2021-08-27 16:47:05 -0700

    Fixed issue with smooth scroll on chromium based browsers

--- static/js/scrollBox.js
+++ static/js/scrollBox.js
@@ -1,66 +1,69 @@
 
 $(document).ready(function(){
-	var contentSection = $('.card__content');
-	
-	// When a nav link is clicked, smooth scroll to the section
-	$("a").on('click', function(event) {
-    event.preventDefault();
-		smoothScroll($(this.hash));
-		//applyActiveClass($(this)[0]);
-	});
-	
-	
-	// Update navigation on scroll...
-	contentSection.on('scroll', function(){
-		updateNavigation();
-	})
-	// ...and when the page starts
-	updateNavigation();
-	
+  var contentSection = $('.card__content');
+  
+  if (!CSS.supports('scroll-behavior', 'smooth')) {
+    // When a nav link is clicked, smooth scroll to the section
+    $("a").on('click', function(event) {
+      event.preventDefault();
+      smoothScroll($(this.hash));
+      //applyActiveClass($(this)[0]);
+  });
+  }
+  
+  
+  // Update navigation on scroll...
+  contentSection.on('scroll', function(){
+    updateNavigation();
+  })
+  // ...and when the page starts
+  updateNavigation();
+  
 
-	function updateNavigation(){
-		const sections = document.getElementsByClassName('card__container');
-		for (let section of sections) {
-			var sectionName = section.id;
-			var navigationMatch = $(`a[href="#${sectionName}"]`)
-			const amountScrolled = contentSection.scrollTop();
-			const sectionHeight = $(section).height();
-			const sectionEnd =  sectionHeight + section.offsetTop;
-			const scrollBoxHeight = contentSection.height();
-			const lowerThreshold = section.offsetTop - (scrollBoxHeight/5);
-			const upperThreshold = sectionEnd - (scrollBoxHeight/5)
-			// Add/remove class based on if scrolling in target section
-			if (lowerThreshold < amountScrolled  && amountScrolled < upperThreshold) {
-				applyActiveClass(navigationMatch);
-			} else {
-				removeActiveClass(navigationMatch);
-			}
-		};
-	}
+  function updateNavigation(){
+    const sections = document.getElementsByClassName('card__container');
+    for (let section of sections) {
+      var sectionName = section.id;
+      var navigationMatch = $(`a[href="#${sectionName}"]`)
+      const amountScrolled = contentSection.scrollTop();
+      const sectionHeight = $(section).height();
+      const sectionEnd =  sectionHeight + section.offsetTop;
+      const scrollBoxHeight = contentSection.height();
+      const lowerThreshold = section.offsetTop - (scrollBoxHeight/5);
+      const upperThreshold = sectionEnd - (scrollBoxHeight/5)
+      // Add/remove class based on if scrolling in target section
+      if (lowerThreshold < amountScrolled  && amountScrolled < upperThreshold) {
+        applyActiveClass(navigationMatch);
+      } else {
+        removeActiveClass(navigationMatch);
+      }
+    };
+  }
 });
 
 
 function smoothScroll(target){
-	const position = target.position().top;
-	($('.card__content')).animate({
-		scrollTop: target[0].offsetTop
-	}, 800);
+  const position = target.position().top;
+  ($('.card__content')).animate({
+    scrollTop: target[0].offsetTop
+  }, 800);
 }
 
 
 function removeActiveClass(button) {
-	// Remove active classes
-	button.removeClass('button--active');
-	Array.from(button.find('path')).forEach((el) => el.classList.remove('button__path--active'));
-	const caption = button.parent().parent().find('.button__caption')
-	caption.removeClass('button__caption--active');
+  // Remove active classes
+  button.removeClass('button--active');
+  Array.from(button.find('path')).forEach((el) => el.classList.remove('button__path--active'));
+  const caption = button.parent().parent().find('.button__caption')
+  caption.removeClass('button__caption--active');
 }
 
 
 function applyActiveClass(button) {
-	// Add active classes to elements children
-	button.addClass('button--active');
-	Array.from(button.find('path')).forEach((el) => el.classList.add('button__path--active'));
-	const caption = button.parent().parent().find('.button__caption');
-	caption.addClass('button__caption--active');
+  // Add active classes to elements children
+  button.addClass('button--active');
+  Array.from(button.find('path')).forEach((el) => el.classList.add('button__path--active'));
+  const caption = button.parent().parent().find('.button__caption');
+  caption.addClass('button__caption--active');
 }
+
